8ca0a23a2b03cc46ad127274b0f7f2259f2b4439
Ryan Kim <80545744+ryankim0709@users.noreply.github.com>
2021-06-30 22:15:30 +0000

    stuff

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 import discord
 import os
 import requests
+from bs4 import BeautifulSoup
 import json
-import random
-from replit import db
 from keep_alive import keep_alive
+
+pagehtml = requests.get("https://shock.overwatchleague.com/en-us/roster").text
+soup = BeautifulSoup(pagehtml, 'html.parser')
+things = soup.find("script", {"id": "__NEXT_DATA__"})
+print(things)
+data = json.loads(list(things)[0])
+
 
 client = discord.Client()
 
@@ -16,6 +22,13 @@ async def on_ready():
 async def on_message(message):
   if message.author == client.user:
     return
+  
+  if(message.content.startswith("-")):
+    if(message.content.startswith("-hello")):
+      await message.channel.send('Hello!')
+    
+    if(message.content.startswith('-roster shock')):
+      await message.channel.send('SF shock roster')
 
 keep_alive()
 my_secret = os.environ['token']
