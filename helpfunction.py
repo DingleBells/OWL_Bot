ae44f9d0298de87542e10cd880251387b038d8a9
kanghee <kanghee.cho7@gmail.com>
2021-07-02 15:08:22 -0700

    added ability to only reply to specific channels

--- helpfunction.py
+++ helpfunction.py
@@ -1,14 +1,44 @@
 import discord
 
+
 def getHelp():
-  helpScreen = discord.Embed(
-    title = "All commands",
-    description = "List of all commands and functions",
-    color = discord.Colour.gold()
-  )
+    helpScreen = discord.Embed(
+        title="All commands",
+        description="List of all commands and functions",
+        color=discord.Colour.gold()
+    )
 
-  helpScreen.add_field(name = "-owl shedule", value = "Gets the schedule for the current week in PST")
+    helpScreen.add_field(name="-owl shedule", value="Gets the schedule for the current week in PST")
 
-  helpScreen.add_field(name = "-roster {team name}", value = "Gets the roster for a given team name. Please type out the full team name")
+    helpScreen.add_field(name="-roster {team name}",
+                         value="Gets the roster for a given team name. Please type out the full team name")
 
-  return helpScreen
+    return helpScreen
+
+
+def isChannelGood(channel):
+    f = open("channels.txt")
+    channel = str(channel)
+    for line in f.readlines():
+        # print(channel, line.strip(), type(channel), type(line))
+        if channel == line.strip():
+            f.close()
+            return True
+    f.close()
+    return False
+
+def getGoodChannels():
+    f = open('channels.txt')
+    outlist = []
+    for line in f.readlines():
+        outlist.append(line.strip())
+    return outlist
+
+def addGoodChannel(channel):
+    f = open("channels.txt", 'a')
+    f.write(f"{channel}\n")
+    f.close()
+    return
+
+def clearGoodChannels():
+    open('channels.txt', 'w').close()
--- main.py
+++ main.py
@@ -1,42 +1,71 @@
 import discord
 import os
-from keep_alive import keep_alive
-from schedule import*
-from displayRoster import *
-from helpfunction import*;
-from getStandings import *;
+# from keep_alive import keep_alive
+from schedule import *
+from getRoster import *
+from helpfunction import *
+from getStandings import *
 
 client = discord.Client()
 
+
 @client.event
 async def on_ready():
-  print('We have loggied in as {0.user}'.format(client))
+    print('We have logged in as {0.user}'.format(client))
+
 
 @client.event
 async def on_message(message):
-  if message.author == client.user:
-    return
-  msg = message.content
-  if(msg.startswith("-")):
-    
-    if(msg.startswith("-roster")): 
-      team = (msg.split("-roster ")[1]).lower()
-      await message.channel.send(findTeam(team))
-    
-    elif msg.startswith("-owl standings"):
-      response = message.content.split()[2]
-      west, east, tourney = owlSchedule(response)
-      print(len(west), len(east))
-      await message.channel.send(tourney)
-      await message.channel.send(west)
-      await message.channel.send(east)
+    if message.author == client.user:
+        return
+    msg = message.content
 
-    elif msg.startswith("-help"):
-      await message.channel.send(embed = getHelp())
+    if msg.startswith("-help"):
+        await message.channel.send(embed=getHelp())
 
-    elif (msg.startswith("-owl schedule")):
-      await message.channel.send(embed=embedSchedule())
+    elif msg.startswith("-"):
+        if msg.startswith("-settings"):
+            restofmsg = msg.split('-settings ')[1]
+            if restofmsg == "whitelist":
+                addGoodChannel(message.channel)
+                await message.channel.send("This channel has been whitelisted.")
 
-keep_alive()
-my_secret = os.environ['token']
+            elif restofmsg == 'get whitelist':
+                await message.channel.send(f"The following channels are whitelisted: {getGoodChannels()}")
+
+            elif restofmsg == 'clear whitelist':
+                clearGoodChannels()
+                await message.channel.send("The whitelist has been cleared!")
+
+        else:
+            if not isChannelGood(message.channel):
+                await message.channel.send(":no_entry_sign: This channel cannot be used for OWL commands.")
+                return
+            else:
+                if msg.startswith("-owl"):
+                    restofmsg = msg.split('-owl ')[1]
+
+                    if restofmsg.startswith('roster'):
+                        team = (restofmsg.split("roster ")[1]).lower()
+                        print(team)
+                        await message.channel.send(findTeam(team))
+
+                    elif restofmsg.startswith("standings"):
+                        response = restofmsg.split()[1]
+                        west, east, tourney = owlSchedule(response)
+                        print(len(west), len(east))
+                        await message.channel.send(tourney)
+                        await message.channel.send(west)
+                        await message.channel.send(east)
+
+                    elif restofmsg == 'schedule':
+                        await message.channel.send(embed=embedSchedule())
+
+
+
+
+
+
+# keep_alive()
+my_secret = 'ODU5OTEzNjU4NjM2OTU5NzQ1.YNznAA.hxRfElkJ9At0FTkH7W9kjK2T97A'
 client.run(my_secret)
