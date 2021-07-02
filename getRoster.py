ffd200d8f52f2fbabae7dd70def6b702609750ad
Ryan Kim <80545744+ryankim0709@users.noreply.github.com>
2021-07-02 22:17:08 +0000

    added so that it will show roster by name. ex roster shock

--- getRoster.py
+++ getRoster.py
@@ -6,6 +6,9 @@ def findTeam(team):
   if(contains(team) == 1):
     return "```"+team+" roster\n"+getRoster(team.split(" ")[1])+"```"
   
+  elif(containsLast(team) == 1):
+    return "```"+team+" roster\n"+getRoster(team)+"```"
+  
 
 def contains(team):
   team_names = ['atlanta reign','boston uprising','dallas fuel',
@@ -14,6 +17,20 @@ def contains(team):
   'new york excelsior','paris eternal','philadelphia fusion',
   'san francisco shock', 'seoul dynasty','shanghai dragons',
   'toronto defiant', 'vancouver titans','washington justice']
+
+  for teams in team_names:
+    if(teams == team):
+      return 1
+  
+  return 0
+
+def containsLast(team) :
+  team_names = ['reign','uprising','fuel',
+  'mayhem','charge','spark','outlaws',
+  'spitfire','gladiators','valiant',
+  'excelsior','eternal','fusion',
+  'shock', 'dynasty','dragons',
+  'defiant', 'titans','justice']
 
   for teams in team_names:
     if(teams == team):
