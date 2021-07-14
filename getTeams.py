202b1934dd6e71ccb404d027356a64d07a61a5b4
Ryan Kim <80545744+ryankim0709@users.noreply.github.com>
2021-07-14 20:14:23 +0000

    TeamList update

---
+++ getTeams.py
@@ -0,0 +1,31 @@
+import discord
+
+def getTeams(reign, uprising, hunters, fuel, mayhem, gladiators, charge, outlaws, spark,
+spitfire, excelsior, eternal, fusion, dynasty, shock, dragons, defiant, valiant, titans,
+justice) :
+    teamList = discord.Embed(
+        title="Overwatch League Teams",
+        description="2021 Season\n"+
+        "**"+reign+"ATL-Atlanta Reign**\n"+
+        "**"+uprising+"BOS-Boston Uprising**\n"+
+        "**"+hunters+"CDH-Chengdu Hunters**\n"+
+        "**"+fuel+"DAL-Dallas Fuel**\n"+
+        "**"+mayhem+"FLA-Florida Mayhem**\n"+
+        "**"+gladiators+"GLA-Los Angeles Gladiators**\n"+
+        "**"+charge+"GZC-Guangzhou Charge**\n"+
+        "**"+outlaws+"HOU-Houston Outlaws**\n"+
+        "**"+spark+"HZS-Hangzhou Spark**\n"+
+        "**"+spitfire+"LDN-London Spitfire**\n"+
+        "**"+excelsior+"NYE-New York Excelsior**\n"+
+        "**"+eternal+"PAR-Paris Eternal**\n"+
+        "**"+fusion+"PHI-Philadelphia Fustion**\n"+
+        "**"+dynasty+"SEO-Seoul Dynasty**\n"+
+        "**"+shock+"SFS-San Francisco Shock**\n"+
+        "**"+dragons+"SHD-Shanghai Dragons**\n"+
+        "**"+defiant+"TOR-Toronto Defiant**\n"
+        "**"+valiant+"VAL-Los Angeles Valiant**\n"+
+        "**"+titans+"VAN-Vancouver Titans**\n"+
+        "**"+justice+"WAS-Washington Justice**",
+        color=discord.Colour.gold()
+    )
+    return teamList
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import discord
+from discord.utils import get
 import os
 from keep_alive import keep_alive
 from schedule import embedSchedule
@@ -7,6 +8,7 @@ from helpfunction import *
 from scrollReaction import handleReaction
 from webscraper import getStandingsEmbed
 from scores import getScoreEmbed
+from getTeams import *
 
 
 client = discord.Client()
@@ -67,6 +69,31 @@ async def on_message(message):
             if not isChannelGood(message.channel):
                 await message.channel.send(":no_entry_sign: This channel cannot be used for OWL commands.")
                 return
+
+            elif msg.startswith("-teamList"):
+              reign = "<:reign:864275520769359882>"
+              uprising = "<:uprising:864275585022427157>"
+              hunters = "<:hunters:864275450032816168>"
+              fuel = "<:fuel:864275397910593536>"
+              mayhem = "<:mayhem:864275488473612299>"
+              gladiators = "<:gladiators:864275436987744307>"
+              charge = "<:charge:864275280985980938>"
+              outlaws = "<:outlaws:864275505766334486>"
+              spark = "<:spark:864275546498531388>"
+              spitfire = "<:spitfire:864275559567196221>"
+              excelsior = "<:excelsior:864275362477506590>"
+              eternal = "<:eternal:864275341619232808>"
+              fusion = "<:fusion:864275415061495839>"
+              dynasty = "<:dynasty:864275320675500052>"
+              shock = "<:shock:864275535336570912>"
+              dragons = "<:dragons:864275306776231948>"
+              defiant = "<:defiant:864275293065445396>"
+              valiant = "<:valiant:864275597630504970>"
+              titans = "<:titans:864275573999927326>"
+              justice = "<:justice:864275469510508574>"
+              await message.channel.send(embed = getTeams(reign, uprising, hunters, fuel, mayhem, gladiators, charge, outlaws, spark,
+              spitfire, excelsior, eternal, fusion, dynasty, shock, dragons, defiant, valiant, titans, justice))
+
             else:
                 if msg.startswith("-owl"):
                     restofmsg = msg.split('-owl ')[1]
